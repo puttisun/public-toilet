03c9f43a556c1ce1d0c9ad898a0f5f9f876f18a5
Thanabardi <Thanabardi@hotmail.com>
2022-02-12 13:52:16 +0700

    add axios

---
+++ toilet-front/src/Toilet.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const Toilet = () => {
+  const [detail, setDetail] = useState()
+  const { roomID } = useParams()
+
+  useEffect(() => {
+    RoomStatus(roomID).then((data) => {
+      setDetail(Object.entries(data))
+      console.log(data)
+    })
+  }, [roomID])
+
+  async function RoomStatus(id) {
+  const res = await axios.get(
+    `https://ecourse.cpe.ku.ac.th/exceed03/api/room-status/1`
+    )
+    return res.data
+  }
+
+  return (
+    <div>
+      test
+      { detail }
+    </div>
+  )
+}
+export default Toilet;
