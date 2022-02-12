61341c37a852815b1f859276e501c513c76f15ce
root <natchanon.space@gmail.com>
2022-02-12 13:43:51 +0700

    add constraints and error code

--- toilet-back/main.py
+++ toilet-back/main.py
@@ -3,16 +3,25 @@ from pymongo import MongoClient
 from pydantic import BaseModel
 from datetime import datetime
 
-client = MongoClient('mongodb://localhost', 27017)
+MAXIMUM_ROOM = 3
 
 app = FastAPI()
-
+client = MongoClient('mongodb://localhost', 27017)
 db = client["mini-project"]
 collection = db["room"]
 
 
 @app.post("/start/{room_number}")
 def start(room_number: int):
+    # check room
+    if room_number not in [i for i in range(1, MAXIMUM_ROOM+1)]:
+        raise HTTPException(404, "Room not found")
+
+    # check if room is currently in use or not
+    room = collection.find({"room": room_number}).sort("_id", -1)[0]
+    if room["done"] == 0:
+        raise HTTPException(400, f"room {room_number} is currently in use")
+
     now = datetime.now()
     collection.insert_one({
         "room": room_number,
@@ -22,16 +31,23 @@ def start(room_number: int):
         "total_time": None
     })
     return {
-        "msg": f"{room_number} is currently in use"
+        "msg": f"room {room_number} is currently in use"
     }
 
 
 @app.post("/end/{room_number}")
 def end(room_number: int):
-    now = datetime.now()
+    # check room
+    if room_number not in [i for i in range(1, MAXIMUM_ROOM+1)]:
+        raise HTTPException(404, "Room not found")
 
+    # check if room is currently in use or not
     room = collection.find({"room": room_number}).sort("_id", -1)[0]
+    if room["done"] == 1:
+        raise HTTPException(400, f"room {room_number} is empty now")
 
+    now = datetime.now()
+    room = collection.find({"room": room_number}).sort("_id", -1)[0]
     collection.update_one(
             {
                 "room": room_number,
@@ -47,13 +63,15 @@ def end(room_number: int):
         )
 
     return {
-        "msg": f"{room_number} is empty"
+        "msg": f"room {room_number} is empty"
     }
 
 @app.get("/room-status/{room_number}")
 def room_status(room_number: int):
+    if room_number not in [i for i in range(1, MAXIMUM_ROOM+1)]:
+        raise HTTPException(404, "Room not found")
+
     room = collection.find({"room": room_number}).sort("_id", -1)[0]
-    
     return {
         "room": room["room"],
         "done": room["done"],
